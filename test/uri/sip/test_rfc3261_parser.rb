3cba758f89265282ad08c1b910a640177a59c968
vxd7 <vxd732@protonmail.com>
2025-11-22 20:47:26 +0500

    feat(sip-uri): tests for sip headers and sip params

--- test/uri/sip/test_rfc3261_parser.rb
+++ test/uri/sip/test_rfc3261_parser.rb
@@ -79,4 +79,69 @@ class URI::SIP::TestRFC3261_Parser < Minitest::Test
     assert_equal(hostname, parsed[:host])
     assert_equal(port.to_s, parsed[:port])
   end
+
+  def test_sip_uri_with_username
+    username = 'alice;day=tuesday'
+    parsed = parse("sip:#{username}@atlanta.com")
+
+    assert_equal(username, parsed[:userinfo])
+  end
+
+  def test_sip_uri_with_ipv4_and_params
+    hostname = '192.168.0.1'
+    params = ';lr;param=value;param1'
+    parsed = parse("sip:#{hostname}#{params}")
+
+    assert_equal(hostname, parsed[:host])
+    assert_equal(params, parsed[:uri_parameters])
+  end
+
+  def test_sip_uri_with_ipv6_and_params
+    hostname = '[::ffff:192.0.2.128]'
+    params = ';lr;param=value;param1'
+    parsed = parse("sip:#{hostname}#{params}")
+
+    assert_equal(hostname, parsed[:host])
+    assert_equal(params, parsed[:uri_parameters])
+  end
+
+  def test_sip_uri_with_ipv4_and_headers
+    hostname = '192.168.0.1'
+    headers = '?X-HEADER1=X-VALUE1&X-HEADER2=X-VALUE2&X-HEADER3=&X-HEADER4='
+    parsed = parse("sip:#{hostname}#{headers}")
+
+    assert_equal(hostname, parsed[:host])
+    assert_equal(headers, parsed[:headers])
+  end
+
+  def test_sip_uri_with_ipv6_and_headers
+    hostname = '[::ffff:192.0.2.128]'
+    headers = '?X-HEADER1=X-VALUE1&X-HEADER2=X-VALUE2&X-HEADER3=&X-HEADER4='
+    parsed = parse("sip:#{hostname}#{headers}")
+
+    assert_equal(hostname, parsed[:host])
+    assert_equal(headers, parsed[:headers])
+  end
+
+  def test_sip_uri_with_ipv4_params_and_headers
+    hostname = '192.168.0.1'
+    params = ';lr;param=value;param1'
+    headers = '?X-HEADER1=X-VALUE1&X-HEADER2=X-VALUE2&X-HEADER3=&X-HEADER4='
+    parsed = parse("sip:#{hostname}#{params}#{headers}")
+
+    assert_equal(hostname, parsed[:host])
+    assert_equal(params, parsed[:uri_parameters])
+    assert_equal(headers, parsed[:headers])
+  end
+
+  def test_sip_uri_with_ipv6_params_and_headers
+    hostname = '[::ffff:192.0.2.128]'
+    params = ';lr;param=value;param1'
+    headers = '?X-HEADER1=X-VALUE1&X-HEADER2=X-VALUE2&X-HEADER3=&X-HEADER4='
+    parsed = parse("sip:#{hostname}#{params}#{headers}")
+
+    assert_equal(hostname, parsed[:host])
+    assert_equal(params, parsed[:uri_parameters])
+    assert_equal(headers, parsed[:headers])
+  end
 end
